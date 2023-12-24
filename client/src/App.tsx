1ffb33a8c7935b842df6542390745537f2ae5b22
Ken Yamada <yamakenth@gmail.com>
2023-12-24 11:16:13 +0700

    Create skeleton homepage for login/signup form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,10 @@
-import { Box, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Box } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 import { ChatPage, Homepage } from "./pages";
 
 const App = () => {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    (async () => {
-      const res = await fetch("/api");
-      const data = await res.json();
-      setMessage(data.message);
-    })();
-  }, []);
-
   return (
     <Box minHeight="100vh" textAlign="center" fontSize="xl">
-      <Text>{message}</Text>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/chats" element={<ChatPage />} />
--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,7 +1,55 @@
-import { Text } from "@chakra-ui/react";
+import { ChatIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Container,
+  Heading,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+} from "@chakra-ui/react";
 
-const Homepage = () => {
-  return <Text>Homepage</Text>;
-};
+const Homepage = () => (
+  <Container bg="gray.100" minH="100vh" minW="100vw">
+    <Container centerContent maxW="xl">
+      <Box
+        bg="white"
+        border="2px"
+        borderColor="gray.200"
+        borderRadius="lg"
+        display="flex"
+        justifyContent="center"
+        m="40px 0 15px 0"
+        p={3}
+        w="100%"
+      >
+        <Heading as="h1" size="xl">
+          <ChatIcon color="teal" />
+          &nbsp;&nbsp;Chat App
+        </Heading>
+      </Box>
+      <Box
+        bg="white"
+        border="2px"
+        borderColor="gray.200"
+        borderRadius="lg"
+        p={4}
+        w="100%"
+      >
+        <Tabs colorScheme="teal">
+          <TabList>
+            <Tab w="50%">Login</Tab>
+            <Tab w="50%">Sign Up</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>Login</TabPanel>
+            <TabPanel>Signup</TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  </Container>
+);
 
 export default Homepage;
